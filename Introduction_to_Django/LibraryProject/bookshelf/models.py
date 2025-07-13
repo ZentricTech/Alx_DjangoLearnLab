fa56eed0dd322d1f4c7f5aa1e4589f95f6df698d
Firmstack <zentrictech@gmail.com>
2025-07-13 20:16:57 +0300

    Alx_DjangoLearnLab

---
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author} in {self.published_year}"
